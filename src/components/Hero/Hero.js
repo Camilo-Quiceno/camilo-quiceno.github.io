74b6bbfabe14660c95b0aac41f732024bd27dd06
camilo-quiceno-nu <camilo.quiceno@nubank.com.br>
2025-08-25 21:23:58 -0500

    Cleaning code

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,23 +2,24 @@ import React from 'react';
 import styles from './Hero.module.css';
 import nuIcon from '../../assets/icons/nu-icon.png';
 
-const HeroDefaultProps = {
-  emoji: '👋',
-  backgroundImage: null
-};
-
-const Hero = ({emoji, title1, title2, title3, backgroundImage}) => {
+const Hero = ({ emoji = '👋', title1, title2, title3, backgroundImage }) => {
   const heroStyle = backgroundImage 
     ? { backgroundImage: `url(${backgroundImage})` }
     : {};
+    
   return (
     <div className={styles.hero} style={heroStyle}>
       <h1 className={styles.emoji}>{emoji}</h1>
       <h1>{title1}</h1>
-      <br></br>
       <h2>{title2}</h2>
       <h3 className={styles.titleWithIcon}>
-        <img src={nuIcon} alt="Nu" className={styles.icon} loading="lazy" decoding="async" />
+        <img 
+          src={nuIcon} 
+          alt="Nu" 
+          className={styles.icon} 
+          loading="lazy" 
+          decoding="async" 
+        />
         {title3}
       </h3>
     </div>
